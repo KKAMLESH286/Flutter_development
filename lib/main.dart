1dfa08c95e4cccbf042462586d00d659e62654ed
kkamlesh28 <kamlesh@dozzbee.com>
2021-02-13 17:42:43 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+
+void main(){
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatefulWidget{
+
+@override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+
+class _MyAppState extends State<MyApp> {
+  
+
+  
+
+  
+    var _questionIndex = 0;
+
+    void _answerQuestion(){
+      setState(()=>{
+        _questionIndex = _questionIndex+1
+      });
+      
+      print(_questionIndex);
+    }
+
+    @override
+  Widget build(BuildContext context){
+
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?',
+      'What\'s your favourite movie?'
+    ];
+    return MaterialApp(home: Scaffold(appBar: AppBar(
+    title: Text('Simple Quiz App', style: TextStyle(fontSize: 25),),
+    centerTitle: true,
+    elevation: 30,
+    
+  ),
+  body: Column(
+    children: [
+    Question(questions[_questionIndex],
+    ),
+
+
+   RaisedButton(child:Text("Answer 1"),
+   onPressed: _answerQuestion,
+   ),
+   RaisedButton(child: Text("Answer 2"),
+    onPressed: _answerQuestion,
+    ),
+   RaisedButton(child: Text("Answer 2"),
+    onPressed: _answerQuestion,
+    ),  
+   ]),
+
+
+    ),
+    theme: ThemeData(primarySwatch: Colors.blue, accentColor:Colors.blue),
+    );
+  }
+}
+
+
