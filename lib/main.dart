ea8d6e34b711d6f29fbe05c2e7ae625817e4e0ba
Kamlesh Panwar <kkamlesh286@gmail.com>
2021-02-21 20:10:24 +0530

    till 51 lecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,68 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import 'quiz.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
-
 }
 
-class MyApp extends StatefulWidget{
-
-@override
+class MyApp extends StatefulWidget {
+  @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _MyAppState();
   }
 }
 
-
 class _MyAppState extends State<MyApp> {
-    final questions = const [
-      {
-      'questionText':'What\'s your favourite color?', 
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favourite color?',
       'answerText': ['Black', 'Red', 'Green', 'White']
-      },
-      {
-        'questionText': 'What\'s your favourite animal?',
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
       'answerText': ['Dog', 'Cat', 'Rabbit']
-      },
-      {
-        'questionText': 'What\'s your favourite movie?',
+    },
+    {
+      'questionText': 'What\'s your favourite movie?',
       'answerText': ['Tenet', 'Pulp Fiction', 'Inception']
-      }
-    ];
+    }
+  ];
 
-  
+  var _questionIndex = 0;
 
-  
-    var _questionIndex = 0;
-    
-    void _answerQuestion(){
-      
-      setState(()=>{
-        _questionIndex = _questionIndex+1
-        
-      });
-      if (_questionIndex < questions.length) {
-          print('We have more questions!');
+  void _answerQuestion() {
+    setState(() => {_questionIndex = _questionIndex + 1});
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
     } else {
       print('No more questions!');
     }
-      
-    }
+  }
 
-    @override
-  Widget build(BuildContext context){
-
-    
-    return MaterialApp(home: Scaffold(appBar: AppBar(
-    title: Text('Simple Quiz App', style: TextStyle(fontSize: 25),),
-    centerTitle: true,
-    elevation: 30,
-    
-  ),
-  body: _questionIndex < questions.length ? Column(
-    children: [
-    Question(
-      questions[_questionIndex]['questionText'],
-    ),
-
-
-    ...(questions[_questionIndex]['answerText'] as List<String>).map((answerText){
-      return Answer(_answerQuestion, answerText);
-    }).toList()  
-   ]): Center(child: Text('You did it'),) ,
-
-
-    ),
-    theme: ThemeData(primarySwatch: Colors.blue, accentColor:Colors.blue),
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Simple Quiz App',
+            style: TextStyle(fontSize: 25),
+          ),
+          centerTitle: true,
+          elevation: 30,
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Center(
+                child: Text('You did it'),
+              ),
+      ),
+      theme: ThemeData(primarySwatch: Colors.blue, accentColor: Colors.blue),
     );
   }
-} 
-
-
+}
